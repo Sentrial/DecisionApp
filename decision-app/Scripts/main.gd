0fa6ee6b25f942f46d84ce0a97f3283060ec5eae
Sentrial <mccarthybrandon1313@gmail.com>
2025-03-10 14:07:41 -0600

    Each option now has its own line edit. More options can be added and each option can be deleted.

--- decision-app/Scripts/main.gd
+++ decision-app/Scripts/main.gd
@@ -1,68 +1,37 @@
 extends Node2D
 
 @onready var itemContainer = $Center/ScrollContainer/VBoxContainer
-@onready var textEdit = $Center/InsertGroup/TextEdit
 @onready var itemTheme = load("res://Resources/Themes/ItemTheme.tres")
-@onready var trashIcon = load("res://Resources/Icons/trash-can-solid.svg")
 @onready var answerLabel = $Center/Answer
 @onready var glitterSFX = $Center/Answer/GlitterSFX
-var items = {}
+@onready var optionPrefab = preload("res://Scenes/option.tscn")
 
 func _ready():
 	pass
 
 
-# Adds an item to the list
-func _on_add_item_pressed():
-	if textEdit.text == "":
-		return
-	
-	# Add new item
-	var newItem = HBoxContainer.new()
-	newItem.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	
-	# Creat text
-	var itemText = Label.new()
-	itemText.text = textEdit.text
-	itemText.size_flags_horizontal = Control.SIZE_EXPAND
-	itemText.theme = itemTheme
-	
-	# Create delete button
-	var button = Button.new()
-	button.icon = trashIcon
-	button.expand_icon = true
-	button.size_flags_horizontal = Control.SIZE_SHRINK_END
-	button.custom_minimum_size = Vector2(50,50)
-	button.pressed.connect(_on_delete_pressed.bind(itemText))
-	
-	newItem.add_child(itemText)
-	newItem.add_child(button)
-	button.alignment = BoxContainer.ALIGNMENT_END
-	itemContainer.add_child(newItem)
-	
-	items[itemText.text] = true
-	
-	textEdit.text = ""
-
-
-# Deletes the item from the list
-func _on_delete_pressed(label: Label):
-	items.erase(label.text)
-	label.get_parent().queue_free()
+func _on_add_option_pressed():
+	var newOption = optionPrefab.instantiate()
+	var pos = max(0, itemContainer.get_child_count() - 1)
+	newOption.get_child(0).placeholder_text = "Option " + str(pos + 1) + " . . ."
+	itemContainer.add_child(newOption)
+	itemContainer.move_child(newOption, pos)
 
 
 # Generates a random answer
 func _on_answer_pressed():
-	var options = items.keys()
-	var answer = "[center][b]. . .[/b][/center]"
+	var options = []
+	var items = itemContainer.get_children()
+	for item in items:
+		for child in item.get_children():
+			if child is LineEdit:
+				if child.text != "":
+					options.append(child.text)
+	
 	if len(options) > 0:
-		answer = "[center][b]" + options[randi() % options.size()] + "[/b][/center]"
+		answerLabel.text = "[center][b]" + options[randi() % options.size()] + "[/b][/center]"
 		glitterSFX.play()
 	
-	answerLabel.text = answer
 
-func _on_clear_pressed():
-	for child in itemContainer.get_children():
-		itemContainer.remove_child(child)
-		child.queue_free()
-	items.clear()
+func _on_reset_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
