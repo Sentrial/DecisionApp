d6d0fa1f74ed2b368767c2bd1bb05e317e74bd9f
Sentrial <mccarthybrandon1313@gmail.com>
2025-03-14 19:56:41 -0600

    Added about scene. Final changes.

--- decision-app/Scripts/main.gd
+++ decision-app/Scripts/main.gd
@@ -35,3 +35,7 @@ func _on_answer_pressed():
 
 func _on_reset_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_about_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/about.tscn")
